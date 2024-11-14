73881a32280b71eb99132b2853ba968b82622b5f
Syed Abu Bakar Mashor Al-Idrus <alidrus@xdms.com>
2024-11-14 16:51:40 +0800

    Changed name of variable heat index target -> target heat index (sounds better)

--- Hysteresis.cpp
+++ Hysteresis.cpp
@@ -6,6 +6,8 @@
 #include "Hysteresis.h"
 
 void Hysteresis::monitorComfort(float currentHeatIndex) {
+    CompressorControl compressorController;
+
     const unsigned long currentTime = millis(); // Function to get current time in seconds
 
     if (this->acStartTime == 0) {
@@ -16,38 +18,38 @@ void Hysteresis::monitorComfort(float currentHeatIndex) {
     bool applyHysteresis = (currentTime - this->acStartTime >= INITIAL_COOLDOWN_TIME);
 
     // Check if compressor should be turned ON
-    if (currentHeatIndex > this->heatIndexTarget && !this->compressorOn && (currentTime - this->compressorRestStartTime >= MIN_REST_TIME)) {
+    if (currentHeatIndex > this->targetHeatIndex && !this->compressorOn && (currentTime - this->compressorRestStartTime >= MIN_REST_TIME)) {
         // Turn compressor ON if heat index is above target and rest time has passed
         this->compressorOn = true;
         this->compressorStartTime = currentTime;
-        turnCompressorOn(); // Send cooling mode signal
+        compressorController.turnCompressorOn(); // Send cooling mode signal
     }
 
     // Check if compressor should be turned OFF
     if (this->compressorOn
-            && ((applyHysteresis && currentHeatIndex <= this->heatIndexTarget - this->hysteresisBuffer) // Apply hysteresis after cooldown time
-                || (!applyHysteresis && currentHeatIndex <= this->heatIndexTarget)                // No hysteresis during initial cooldown
+            && ((applyHysteresis && currentHeatIndex <= this->targetHeatIndex - this->hysteresisBuffer) // Apply hysteresis after cooldown time
+                || (!applyHysteresis && currentHeatIndex <= this->targetHeatIndex)                // No hysteresis during initial cooldown
                 || (currentTime - this->compressorStartTime >= MAX_RUN_TIME)))                    // Max runtime safety cutoff
     {
         // Turn compressor OFF if any of the above conditions are met
         this->compressorOn = false;
         this->compressorRestStartTime = currentTime; // Start rest timer
-        turnCompressorOff(); // Send fan mode signal
+        compressorController.turnCompressorOff(); // Send fan mode signal
     }
 }
 
-float Hysteresis::getHeatIndexTarget() const {
-    return this->heatIndexTarget;
+float Hysteresis::getTargetHeatIndex() const {
+    return this->targetHeatIndex;
 }
 
-void Hysteresis::setHeatIndexTarget(unsigned long heatIndexTarget) {
-    this->heatIndexTarget = heatIndexTarget;
+void Hysteresis::setTargetHeatIndex(unsigned long targetHeatIndex) {
+    this->targetHeatIndex = targetHeatIndex;
 }
 
-void Hysteresis::incrementHeatIndexTarget() {
-    this->heatIndexTarget += 0.5;
+void Hysteresis::incrementTargetHeatIndex() {
+    this->targetHeatIndex += 0.5;
 }
 
-void Hysteresis::decrementHeatIndexTarget() {
-    this->heatIndexTarget -= 0.5;
+void Hysteresis::decrementTargetHeatIndex() {
+    this->targetHeatIndex -= 0.5;
 }
--- Hysteresis.h
+++ Hysteresis.h
@@ -10,36 +10,36 @@
 // Configurable limits
 #define MAX_RUN_TIME 360000                   // Max compressor run time in milliseconds (7 minutes)
 #define MIN_REST_TIME 120000                  // Min compressor rest time in milliseconds (3 minutes)
-#define INITIAL_COOLDOWN_TIME 1200000         // Initial pre-hysteresis cooling period in milliseconds (20 minutes)
+#define INITIAL_COOLDOWN_TIME 900000          // Initial pre-hysteresis cooling period in milliseconds (15 minutes)
 
 class Hysteresis
 {
     public:
-        Hysteresis(float heatIndexTarget = 26, float hysteresisBuffer = 0.5)
+        Hysteresis(float targetHeatIndex = 26, float hysteresisBuffer = 0.5)
         {
-            this->heatIndexTarget = heatIndexTarget;
+            this->targetHeatIndex = targetHeatIndex;
             this->hysteresisBuffer = hysteresisBuffer;
         }
 
         void monitorComfort(float currentHeatIndex);
 
-        float getHeatIndexTarget() const;
+        float getTargetHeatIndex() const;
 
-        void setHeatIndexTarget(unsigned long heatIndexTarget);
+        void setTargetHeatIndex(unsigned long targetHeatIndex);
 
-        void incrementHeatIndexTarget();
+        void incrementTargetHeatIndex();
 
-        void decrementHeatIndexTarget();
+        void decrementTargetHeatIndex();
 
     protected:
-        float heatIndexTarget;                   // "Feels-like" target
-        float hysteresisBuffer = 0.5;                 // Buffer to prevent rapid cycling after initial cooldown
+        float targetHeatIndex;                      // Heat index is what the temperature "feels like"
+        float hysteresisBuffer;                     // Buffer to prevent rapid cycling after initial cooldown
 
-        bool compressorOn = false;
+        bool compressorOn = false;                  // Is the compressor running?
 
-        unsigned long compressorStartTime = 0;
-        unsigned long compressorRestStartTime = 0;
-        unsigned long acStartTime = 0; // Track the overall start time of the A/C
+        unsigned long compressorStartTime = 0;      // When the compressor started running
+        unsigned long compressorRestStartTime = 0;  // When the compressor started resting
+        unsigned long acStartTime = 0;              // Track the overall start time of the A/C
 };
 
 #endif
