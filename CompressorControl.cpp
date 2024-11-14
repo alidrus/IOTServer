233e7c438de52f57c15d6bdf8eca46f6591d5a99
Syed Abu Bakar Mashor Al-Idrus <alidrus@xdms.com>
2024-11-14 21:12:57 +0800

    Added serial debug messages

--- CompressorControl.cpp
+++ CompressorControl.cpp
@@ -7,17 +7,27 @@
 #include "CompressorControl.h"
 #include "Pins.h"
 
+
 void CompressorControl::turnACOnOff() {
+    Serial.println("CompressorControl::turnACOnOff() executing irsend.sendRaw(txPowerToggle)");
+
     IRsend irsend(IR_PIN);
+
     irsend.sendRaw(this->txPowerToggle, sizeof(this->txPowerToggle) / sizeof(this->txPowerToggle[0]), IR_CARRIER_FREQUENCY);
 }
 
 void CompressorControl::turnCompressorOn() {
+    Serial.println("CompressorControl::turnCompressorOn() executing irsend.sendRaw(txCoolingMode)");
+
     IRsend irsend(IR_PIN);
+
     irsend.sendRaw(this->txCoolingMode, sizeof(this->txCoolingMode) / sizeof(this->txCoolingMode[0]), IR_CARRIER_FREQUENCY);
 }
 
 void CompressorControl::turnCompressorOff() {
+    Serial.println("CompressorControl::turnCompressorOff() executing irsend.sendRaw(txFanMode)");
+
     IRsend irsend(IR_PIN);
+
     irsend.sendRaw(this->txFanMode, sizeof(this->txFanMode) / sizeof(this->txFanMode[0]), IR_CARRIER_FREQUENCY);
 }
