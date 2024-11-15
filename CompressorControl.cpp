2331733be93de199068b616a82d02725f72acca3
Syed Abu Bakar Mashor Al-Idrus <alidrus@xdms.com>
2024-11-15 21:48:20 +0800

    Updated to use the newer format of IRremote

--- CompressorControl.cpp
+++ CompressorControl.cpp
@@ -1,33 +1,31 @@
 // vim: syntax=arduino autoindent expandtab tabstop=4 shiftwidth=4 softtabstop=4:
 
 #define COMPRESSOR_CONTROL_CPP
+#define USE_IRREMOTE_HPP_AS_PLAIN_INCLUDE
 
-#include <IRremote.h>
-
-#include "CompressorControl.h"
 #include "Pins.h"
+#include "CompressorControl.h"
 
+#include <IRremote.hpp>
+
+CompressorControl::CompressorControl() {
+    IrSender.begin();
+}
 
 void CompressorControl::turnACOnOff() {
-    Serial.println("CompressorControl::turnACOnOff() executing irsend.sendRaw(txPowerToggle)");
+    Serial.println("CompressorControl::turnACOnOff() executing IrSender.sendRaw(txPowerToggle)");
 
-    IRsend irsend(IR_PIN);
-
-    irsend.sendRaw(this->txPowerToggle, sizeof(this->txPowerToggle) / sizeof(this->txPowerToggle[0]), IR_CARRIER_FREQUENCY);
+    IrSender.sendRaw(this->txPowerToggle, sizeof(this->txPowerToggle) / sizeof(this->txPowerToggle[0]), NEC_KHZ);
 }
 
 void CompressorControl::turnCompressorOn() {
-    Serial.println("CompressorControl::turnCompressorOn() executing irsend.sendRaw(txCoolingMode)");
+    Serial.println("CompressorControl::turnCompressorOn() executing IrSender.sendRaw(txCoolingMode)");
 
-    IRsend irsend(IR_PIN);
-
-    irsend.sendRaw(this->txCoolingMode, sizeof(this->txCoolingMode) / sizeof(this->txCoolingMode[0]), IR_CARRIER_FREQUENCY);
+    IrSender.sendRaw(this->txCoolingMode, sizeof(this->txCoolingMode) / sizeof(this->txCoolingMode[0]), NEC_KHZ);
 }
 
 void CompressorControl::turnCompressorOff() {
-    Serial.println("CompressorControl::turnCompressorOff() executing irsend.sendRaw(txFanMode)");
+    Serial.println("CompressorControl::turnCompressorOff() executing IrSender.sendRaw(txFanMode)");
 
-    IRsend irsend(IR_PIN);
-
-    irsend.sendRaw(this->txFanMode, sizeof(this->txFanMode) / sizeof(this->txFanMode[0]), IR_CARRIER_FREQUENCY);
+    IrSender.sendRaw(this->txFanMode, sizeof(this->txFanMode) / sizeof(this->txFanMode[0]), NEC_KHZ);
 }
